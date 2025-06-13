3a1a0062e194779df12750fbe607760fc2edf937
hardik123890 <hardikjain123890@gmail.com>
2025-06-13 19:00:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello React</h1>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
