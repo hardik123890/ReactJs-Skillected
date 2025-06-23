66cb008a98a6ceca60bc968d8a9a4571ebf40084
hardik123890 <hardik123890@gmail.com>
2025-06-23 15:19:03 +0530

    NavigationBar Compleated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Element from './CreateElementsByProps'
 import Footer from './Footer'
 import { MultipleExport } from './MultipleExport'
 import Navbar from './Navbar'
+import UseEffect from './UseEffect'
+import UseStateComponent from './UseState'
+
+
 import { NavbarData } from './data'
 function App() {
 
@@ -13,12 +17,8 @@ function App() {
     <>
      
       <Navbar data={NavbarData} />
-      <Footer title ='hello from footer props' parent="App file" />
-      <Element type="text" placeholder="Enter your name" className="lg"/>
-      <Element type="date" placeholder="Enter your Email" className="sm"/>
-      <Element type="radio" placeholder="" className="sm"/>
-
-      {/* <MultipleExport/> */}
+      
+      <UseEffect/>
     </>
   )
 }
